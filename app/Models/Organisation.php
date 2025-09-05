b561fef4e72cced2451754a404d57d0282eb1b50
Mohamad Alasaly <al-asali@trivio.ru>
2025-09-05 20:01:19 +0300

    Add factory for Organisation

--- app/Models/Organisation.php
+++ app/Models/Organisation.php
@@ -5,6 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property int $id
+ * @property string $name
+ * @property string $phone_number
+ * @property int $building_id
+ */
 class Organisation extends Model
 {
     use HasFactory;
